27b6f2713d53be7879123e8289982a1678f63f56
Dongyue Xu <xdy@bu.edu>
2022-09-29 15:29:45 -0400

    making changes

---
+++ server/routes/posts.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {createPost, getPosts, updatePost, deletePost, likePost, getPostsBySearch, getPost} from '../controllers/posts.js'
+import auth from '../middleware/auth.js';
+
+const router = express.Router();
+
+router.get('/', getPosts);
+router.get('/search', getPostsBySearch);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id',auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.get('/:id', getPost);
+
+export default router;
